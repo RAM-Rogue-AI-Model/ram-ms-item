e4e2e46ac5cc076b11247ed3493f1d7580b27a69
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-02-03 21:02:54 +0100

    openapi.yml

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import { ItemController } from './controllers/ItemController';
 import { ItemRouter } from './routes/itemRouter';
 import { ItemService } from './services/itemService';
 import { config } from './utils/config';
+import fs from 'node:fs';
+import * as YAML from 'yaml';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const port = config.PORT;
@@ -17,7 +20,13 @@ const itemController = new ItemController(itemService);
 
 app.use('/item', new ItemRouter(itemController).router);
 
+const file = fs.readFileSync('./openapi.yml', 'utf8');
+const swaggerDocument = YAML.parse(file) as object;
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`docs available at http://localhost:${port}/docs`);
 });
