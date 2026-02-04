54a8a1436b7a37eff399b9558a75760ed2b6e4f2
Ian Bertin <ian.bertin2004@gmail.com>
2026-02-04 16:46:22 +0100

    Ajout du middleware RequestDetails

--- src/routes/itemRouter.ts
+++ src/routes/itemRouter.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
 
 import { ItemController } from '../controllers/ItemController';
-import { authenticate } from '../utils/auth';
+import { authenticate, requestDetails } from '../utils/auth';
 
 class ItemRouter {
   public router: Router;
@@ -11,22 +11,22 @@ class ItemRouter {
 
     this.router
       .route('/')
-      .post(authenticate, async (req, res) => {
+      .post(requestDetails, authenticate,async (req, res) => {
         await itemController.create(req, res);
       })
-      .get(authenticate, async (req, res) => {
+      .get(requestDetails, authenticate,async (req, res) => {
         await itemController.getAll(req, res);
       });
 
     this.router
       .route('/:id')
-      .get(authenticate, async (req, res) => {
+      .get(requestDetails, authenticate,async (req, res) => {
         await itemController.getById(req, res);
       })
-      .put(authenticate, async (req, res) => {
+      .put(requestDetails, authenticate,async (req, res) => {
         await itemController.update(req, res);
       })
-      .delete(authenticate, async (req, res) => {
+      .delete(requestDetails, authenticate,async (req, res) => {
         await itemController.delete(req, res);
       });
   }
