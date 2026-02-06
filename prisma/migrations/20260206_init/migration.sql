6a5d9f8cf01364cd81dce84ae6d5fee5861884ed
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-02-06 20:19:42 +0100

    fix data

--- prisma/migrations/20260206_init/migration.sql
+++ prisma/migrations/20260206_init/migration.sql
@@ -4,10 +4,9 @@ CREATE TABLE `Item` (
     `name` VARCHAR(50) NOT NULL,
     `description` VARCHAR(50) NOT NULL,
     `price` DECIMAL(15, 2) NOT NULL,
-    `level_gap` VARCHAR(50) NOT NULL,
+    `level_gap` INTEGER NOT NULL,
     `effect_name` VARCHAR(50) NOT NULL,
     `effect_id` VARCHAR(50) NOT NULL,
-
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
